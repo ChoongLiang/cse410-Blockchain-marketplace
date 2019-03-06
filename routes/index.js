7e0ba63fb4910539961e0ccb06087b7dd84dd67d
choong liang <choongli@buffalo.edu>
2019-03-06 11:04:35 -0500

    User(seller) now can add products
    Updates:
    [x] Create a product model
    [x] Connect dashboard product to database
    [x] Product auto update on homepage table
    
    Fixes:
    [x] Button color changed to green for availability
    [x] Removed picture upload
    [x] Item id now auto generated instead of generating in sequence

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const { ensureAuthenticated } = require('../config/auth');
 const mongodb = require('mongodb');
 const currentUser = require('../config/passport');
 
+// Load Product model
+const Product = require('../models/product');
+
 /* GET homepage. */
 router.get('/', function (req, res) {
 
@@ -36,7 +39,10 @@ router.get('/', function (req, res) {
                         data: result
                     });
                 } else {
-                    res.send('No products found');
+                    res.render('index', {
+                        title: 'Big Market',
+                        data: result
+                    });
                 }
                 //Close connection
                 db.close();
@@ -52,10 +58,24 @@ router.get('/dashboard', ensureAuthenticated, (req, res) =>
   })
 );
 
-// router.post('/dashboard',ensureAuthenticated,function (req, res){
-//     const {name, price} = req.body;
-//
-// });
+// Generate new product
+router.post('/dashboard',ensureAuthenticated,function (req, res){
+    const {name, price} = req.body;
+
+    const newProduct = new Product({
+        sellerID: req.user.username,
+        name: name,
+        price: price,
+    });
+
+    newProduct
+        .save()
+        .then(product => {
+            console.log(product);
+            res.redirect('/index-active');
+        });
+
+});
 
 
 
@@ -65,10 +85,45 @@ router.get('/about', function (req, res) {
 });
 
 // Index when logged in
-router.get('/index-active', ensureAuthenticated, (req, res) =>
-    res.render('index-active', {
-        user: req.user, title: 'Big Market'})
-);
+router.get('/index-active', ensureAuthenticated,function (req, res){
+
+    var MongoClient = mongodb.MongoClient;
+    var url = 'mongodb://localhost/blockchain_market';
+
+    // Connect to the server
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+            console.log('Unable to connect to the Server', err);
+        } else {
+            // Connected
+            console.log('Connection established to', url);
+
+            // In order to pull data from database,
+            // We need to get the entire database first,
+            // Then from there we take the collections we want.
+            const productDatabase = db.db('blockchain_market');
+            var collection = productDatabase.collection('products');
+
+            // Find all students
+            collection.find({}).toArray(function (err, result) {
+                if (err) {
+                    res.send(err);
+                } else if (result.length) {
+                    // Make result global
+                    global.datafromDB = result;
+                    res.render('index-active', {
+                        title: 'Big Market',
+                        data: result
+                    });
+                } else {
+                    res.send('No products found');
+                }
+                //Close connection
+                db.close();
+            });
+        }
+    });
+});
 
 // About when logged in
 router.get('/about-active', ensureAuthenticated, (req, res) =>
